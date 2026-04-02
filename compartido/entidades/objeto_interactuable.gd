c45b6a7aa00a2d7111cf8e4bc5c22cb59ee0a2e9
FranNavarro27F <fran27dev@gmail.com>
2026-04-02 20:28:18 -0300

    funciona el multiplayer, ahora añado primer objeto testeo interactuable

---
+++ compartido/entidades/objeto_interactuable.gd
@@ -0,0 +1,7 @@
+extends Node3D
+
+# Esta función será llamada por el jugador cuando presione "E"
+func interactuar():
+	print("¡Has tocado un objeto en Aetherys!")
+	# Ejemplo: que el objeto desaparezca (se recolecte)
+	# queue_free()
